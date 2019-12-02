eb2e9a400f1a1debc6c54ed97d126467857f24ed
Nathan Hawks <nhawks@gmail.com>
2019-12-02 11:34:10 -0800

    Improving layout relationship between main title and Navigator

--- assets/js/ckeditor/config.js
+++ assets/js/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
-
+	config.resize_enabled = false;
 	config.extraPlugins = 'font,colorbutton,colordialog,autolink,find,justify,emoji,save';
 	config.removePlugins = 'specialchar,about';
 	// The toolbar groups arrangement, optimized for two toolbar rows.
