b3c8fd4d521c4f1614b41a5e3e1409d3293f4f13
Nathan Hawks <nhawks@gmail.com>
2019-12-05 07:39:35 -0800

    Sort story-attached items; prevent drag on shaky click; don't gruntwatch

---
+++ api/controllers/common/save-sequence.js
@@ -0,0 +1,55 @@
+module.exports = {
+  friendlyName: 'Save sequence',
+  description: '',
+  inputs: {
+    items: {
+      type: 'ref',
+      required: true
+    }
+  },
+  exits: {
+    success: {
+      responseType: 'view',
+      viewTemplatePath: 'common/save-sequence'
+    }
+  },
+  fn: async function ({items}) {
+    //items = JSON.parse(items);
+    console.log(JSON.stringify(items));
+    for (let x = 0; x < items.length; x++) {
+      let r = [];
+      let f = [];
+      f[x] = async (item, x) => {
+        let id = item.id;
+        let type = item.type;
+        let seq = item.sequence;
+        let sID = item.storyID;
+        switch (type) {
+          case 'Character':
+            r[x] = Promise.resolve(await StoryCharacter.update(
+              {character: id, story: sID})
+              .set({sequence: seq}).fetch());
+          break;
+          case 'Event':
+            r[x] = Promise.resolve(await StoryEvent.update({
+              event: id, story: sID})
+              .set({sequence: seq}).fetch());
+          break;
+          case 'Setting':
+            r[x] = Promise.resolve(await StorySetting.update({
+              setting: id, story: sID})
+              .set({sequence: seq}).fetch());
+          break;
+          // case 'Location':
+          //   r[x] = Promise.resolve(await Location.update({id: id})
+          //     .set({sequence: seq}).fetch());
+          // break;
+        }
+      }; f[x](items[x], x);
+    }
+    return;
+
+  }
+
+
+};
--- api/controllers/story/workspace.js
+++ api/controllers/story/workspace.js
@@ -20,25 +20,41 @@ module.exports = {
 
   fn: async function ({storyID}) {
     var s = await Story.findOne({id: storyID})
-      .populate('mainTitle').populate('summary').populate('elevatorPitch');
-
+      .populate('mainTitle')
+      .populate('summary')
+      .populate('elevatorPitch');
+    // need this to stay in scope
+    var x = 0;
     // populate Navigator - characters
-    var storyCharacters = await StoryCharacter.find({story: storyID});
-    var characterDBIDs = [];
-    storyCharacters.map((sc) => {
-      characterDBIDs[characterDBIDs.length] = sc.character;
-    });
-    var characters = await Character.find({id: characterDBIDs})
-      .populate('realName').populate('codeName');
-
+    var storyCharacters = await StoryCharacter.find({
+      where: {story: storyID}, sort: 'sequence ASC'});
+    var characters = [];
+    // store the functions or the promises will be broken
+    var f1 = [];
+    for (x = 0; x < storyCharacters.length; x++) {
+      let charID = storyCharacters[x].character;
+      f1[x] = async (x, charID, characters) => {
+        characters[x] = await Character.findOne({id: charID})
+          .populate('realName').populate('codeName');
+        return Promise.resolve(characters[x]);
+      }
+      characters[x] = await f1[x](x, charID, characters);
+    }
     // populate Navigator - settings
-    var storySettings = await StorySetting.find({story: storyID});
-    settingDBIDs = [];
-    storySettings.map((ss) => {
-      settingDBIDs[settingDBIDs.length] = ss.setting;
-    });
-    var settings = await Setting.find({id: settingDBIDs})
-      .populate('authorTitle').populate('newsTitle').populate('colloqTitle');
+    var storySettings = await StorySetting.find({
+      where: {story: storyID}, sort: 'sequence ASC'});
+    var settings = [];
+    // store another batch of functions
+    var f2 = [];
+    for (x = 0; x < storySettings.length; x++) {
+      let settID = storySettings[x].setting
+      f2[x] = async (x, settID, settings) => {
+        settings[x] = await Setting.findOne({id: settID})
+          .populate('authorTitle').populate('newsTitle').populate('colloqTitle');
+        return Promise.resolve(settings[x]);
+      }
+      settings[x] = await f2[x](x, settID, settings);
+    }
 
     // populate Navigator - locations
     // var storyLocations = await StoryLocation.find({story: storyID});
@@ -49,13 +65,20 @@ module.exports = {
     // var locations = await Location.find({id: locationDBIDs});
 
     // populate Navigator - events
-    var storyEvents = await StoryEvent.find({story: storyID});
-    eventDBIDs = [];
-    storyEvents.map((se) => {
-      eventDBIDs[eventDBIDs.length] = se.event;
+    var storyEvents = await StoryEvent.find({
+      where: {story: storyID}, sort: 'sequence ASC'
     });
-    var events = await Event.find({id: eventDBIDs})
-      .populate('authorTitle').populate('newsTitle').populate('colloqTitle');
+    events = [];
+    f3 = [];
+    for (x = 0; x < storyEvents.length; x++) {
+      evntID = storyEvents[x].event;
+      f3[x] = async (x,evntID,events) => {
+        events[x] = await Event.findOne({id: evntID})
+          .populate('authorTitle').populate('newsTitle').populate('colloqTitle');
+        return Promise.resolve(events[x]);
+      }; events[x] = await f3[x](x, evntID, events);
+    }
+
     return {
       storyID: storyID,
       story: s,
