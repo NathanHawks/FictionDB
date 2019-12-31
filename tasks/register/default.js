8e472d83a377818c3abd43d8d7f8438962ab6e69
NathanHawks <nhawks@gmail.com>
2019-12-31 02:14:04 +0000

    Finished purging Babel

--- tasks/register/default.js
+++ tasks/register/default.js
@@ -16,11 +16,9 @@ module.exports = function (grunt) {
 
 
   grunt.registerTask('default', [
-    //'polyfill:dev', //« uncomment to ALSO transpile during development (for broader browser compat.)
     'compileAssets',
-    //'babel',        //« uncomment to ALSO transpile during development (for broader browser compat.)
     'linkAssets',
-    // 'watch'
+    'watch'
   ]);
 
 
